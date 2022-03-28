764097e107eb7bcacd27eacca280e55fbcc76c80
Barchynai <bar4inay@gmail.com>
2022-03-28 19:27:03 +0600

    Counter with Redux

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import {store} from '../store/store'
+import { useSelector, useDispatch} from 'react-redux'
 import * as actions from '../store/actions/actionss'
 
 function Counter(){
-    const [refresh, setRefresh] = useState(false)
-    const counter = store.getState()
-    const handlerClick =()=>{
-       store.dispatch({type:actions.INCREASE})
-       setRefresh(!refresh)
-       
+    const counter = useSelector((state) => {
+        return state
+    })
+    
+    const dispatch = useDispatch()
+    const handlerClick = () =>{
+       dispatch({type:actions.INCREASE})
     }
     return (
         <>
